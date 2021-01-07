06fa94e9f0cc1f5b6c9fee8cf2c74a0c7f6742c2
Ahmed ALi <36568764+ahmedali20@users.noreply.github.com>
2021-01-07 18:29:03 +0200

    Create Diagonal Difference.java

---
+++ Problem Solving/Diagonal Difference.java
@@ -0,0 +1,35 @@
+
+import java.math.*;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Solution {
+   public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a[][] = new int[n][n];
+
+        int d1 = 0;
+        int d2 = 0;
+
+        for (int i=0;i<n;i++){
+            for (int j =0;j<n;j++){
+                int m =scanner.nextInt();
+                if (m >= -100 & m<= 100){
+                a[i][j]=m;}
+                if (i == j)
+                    d1+=m;
+            }
+        }
+        int x=n-1;
+        for (int i =0;i<n;i++){
+            for (int j=n-1;j>=0;j--){
+                if (x == j)
+                    d2+=a[i][j];
+            }
+            x--;
+        }
+        System.out.println(Math.abs(d1-d2));
+    }
+}
