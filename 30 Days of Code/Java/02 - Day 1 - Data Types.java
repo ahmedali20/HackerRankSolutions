b532eabca24e433289332750c4c5f119108cc58d
Ahmed ALi <36568764+ahmedali20@users.noreply.github.com>
2021-01-07 14:59:43 +0200

    Create 02 - Day 1 - Data Types.java

---
+++ 30 Days of Code/Java/02 - Day 1 - Data Types.java
@@ -0,0 +1,16 @@
+
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.0");
+
+        int a = scan.nextInt();
+        double b = scan.nextDouble();
+        scan.nextLine();
+        String c=  scan.nextLine();
+        String result=s+c;
+
+
+       System.out.println(a + i);
+       System.out.println(decimalFormat.format(b + d));
+       System.out.println(result);
+
+
