9e638ffada771325b15809cef762fe08e5e0f401
Ahmed ALi <36568764+ahmedali20@users.noreply.github.com>
2021-01-08 04:08:10 +0200

    Create Grading Students.java

---
+++ Algorithms/Implementation/Grading Students.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] grade = new int[n];
+            for (int i = 0; i < grade.length; i++) {
+                grade[i] = scanner.nextInt();
+                    if (grade[i] < 38) {
+                        System.out.println(grade[i]);
+                    } else if (grade[i] % 5 < 3) {
+                        System.out.println(grade[i]);
+                    } else {
+                        grade[i] = grade[i] + (5 - grade[i] % 5);
+                        System.out.println(grade[i]);
+                    }
+                }
+            }
+        }
